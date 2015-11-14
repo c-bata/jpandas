0e1f7fa4bf3b134dd9a978a1bf5d9c20854bd4ed
c-bata <contact@c-bata.link>
2015-11-14 16:15:35 +0900

    Add sample for series

--- app/src/main/java/com/c_bata/javadataframe/MainActivity.java
+++ app/src/main/java/com/c_bata/javadataframe/MainActivity.java
@@ -5,9 +5,14 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.c_bata.Series;
+
+import java.util.Iterator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +31,16 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        // Checking jpandas
+        Series series = new Series();
+        series.add(3);
+        series.add(5);
+
+        Iterator itr = series.iterator();
+        while (itr.hasNext()) {
+            Log.d("JPANDAS", (String) itr.next());
+        }
     }
 
     @Override
