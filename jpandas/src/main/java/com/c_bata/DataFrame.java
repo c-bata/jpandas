5feca3acf5b8f6e33eb7c169478bcfdc08d516ac
c-bata <contact@c-bata.link>
2015-11-14 18:17:17 +0900

    Fake it: describe method

--- jpandas/src/main/java/com/c_bata/DataFrame.java
+++ jpandas/src/main/java/com/c_bata/DataFrame.java
@@ -1,5 +1,6 @@
 package com.c_bata;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class DataFrame<K, S> extends HashMap<K, S> {
@@ -17,5 +18,20 @@ public class DataFrame<K, S> extends HashMap<K, S> {
             meanDataFrame.put((String) key, ((Series) this.get(key)).std());
         }
         return meanDataFrame;
+    }
+
+    public String describe() {
+        StringBuilder sb = new StringBuilder();
+        ArrayList<String> keys = new ArrayList<String>();
+        for (K key : this.keySet()) {
+            keys.add((String) key);
+        }
+        // TODO: Display actual value
+        sb.append("Key"   + "\t\t" +    "a\t" + "b\n");
+        sb.append("count" + "\t\t" +    "3\t" + "3\n");
+        sb.append("mean"  + "\t\t" +  "0.5\t" + "3\n");
+        sb.append("std"   + "\t\t" + "0.25\t" + "3\n");
+
+        return new String(sb);
     }
 }
